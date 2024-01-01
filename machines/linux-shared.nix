f7c1d165b37d25840752c67155b75ed20c0656b7
Terrence Lam <terrence@tlam.dev>
2024-01-01 12:38:01 +0100

    Try greetd

--- machines/linux-shared.nix
+++ machines/linux-shared.nix
@@ -67,6 +67,16 @@
   # Enable the OpenSSH daemon.
   services.openssh.enable = true;
 
+  services.greetd = {
+    enable = true;
+    settings = {
+      default_session = {
+        command = "${pkgs.greetd.tuigreet}/bin/tuigreet --time --cmd sway";
+        user = "terrencelam";
+      };
+    };
+  };
+
   # This value determines the NixOS release from which the default
   # settings for stateful data, like file locations and database versions
   # on your system were taken. It‘s perfectly fine and recommended to leave
