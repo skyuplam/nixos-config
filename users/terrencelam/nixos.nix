fbdd8170454c0e6e47b8e72f3edf81a860e6b733
Terrence Lam <terrence@tlam.dev>
2024-01-01 10:37:25 +0100

    Set authorizedKeys for root

--- users/terrencelam/nixos.nix
+++ users/terrencelam/nixos.nix
@@ -2,11 +2,13 @@
   pkgs,
   config,
   ...
-}: {
+}: let
+  keys = ["ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIIi4mBqMk32PKYVGFJZBXqM+b6vw8b3J0pSFBGAQm3ps TlamM2"];
+in {
   # Add ~/.local/bin to PATH
   environment.localBinInPath = true;
 
-  # Since we're using fish as our shell
+  # Since we're using zsh as our shell
   programs.zsh.enable = true;
 
   users.groups.terrencelam = {};
@@ -19,6 +21,7 @@
     shell = pkgs.zsh;
     hashedPasswordFile = config.sops.secrets.hashedPassword.path;
 
-    openssh.authorizedKeys.keys = ["ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIIi4mBqMk32PKYVGFJZBXqM+b6vw8b3J0pSFBGAQm3ps TlamM2"];
+    openssh.authorizedKeys.keys = keys;
   };
+  users.users.root.openssh.authorizedKeys.keys = keys;
 }
