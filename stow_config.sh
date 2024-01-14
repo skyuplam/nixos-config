ec286aa37e224987ab4395bdc62b8697db530515
Terrence Lam <terrence@tlam.dev>
2024-01-14 11:30:34 +0100

    fix: hyprland conf

--- stow_config.sh
+++ stow_config.sh
@@ -15,5 +15,5 @@ stow_config() {
   mkdir -p "$TARGET"
   # Perform unrestricted find all the symbolic links in the foler to execute rm
   # ${pkgs.fd}/bin/fd -u -t l . $TARGET -x rm
-  stow -d ./users/terrencelam/config -t "$TARGET" -R "$1"
+  stow -d ./users/terrencelam/config -t "$TARGET" "$1"
 }
--- users/terrencelam/home-manager.nix
+++ users/terrencelam/home-manager.nix
@@ -130,34 +130,6 @@ in {
       path = ./config/inputrc;
     };
 
-    # activation = {
-    #   linkNeovimConfig = lib.hm.dag.entryAfter ["writeBoundary"] ''
-    #     #!/bin/bash
-    #
-    #     stow_config() {
-    #       if [ "$#" -lt 1 ]
-    #       then
-    #         echo "stow_config requires at least 1 argument"
-    #         exit 1
-    #       fi
-    #
-    #       local CONFIG_HOME=~/.config
-    #       local TARGET=$CONFIG_HOME/$1
-    #
-    #       echo "linking $1 config to $TARGET"
-    #       # Ensure the folder is there
-    #       mkdir -p $TARGET
-    #       # Perform unrestricted find all the symbolic links in the foler to execute rm
-    #       ${pkgs.fd}/bin/fd -u -t l . $TARGET -x rm
-    #       ${pkgs.stow}/bin/stow -d ${dotfilesPath} -t $TARGET -S $1
-    #     }
-    #
-    #     stow_config "nvim"
-    #     stow_config "eww"
-    #     stow_config "hypr"
-    #   '';
-    # };
-
     packages =
       [
         pkgs.aspell
@@ -202,6 +174,8 @@ in {
         pkgs.sops
         pkgs.age
         pkgs.mkpasswd
+        pkgs.socat
+        pkgs.drill
 
         # Dev stuff
         pkgs.jq
