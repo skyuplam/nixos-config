7a98ff3c342f578c803346e2b0e19d16249beef5
Terrence Lam <terrence@tlam.dev>
2024-01-19 22:41:14 +0100

    feat(anyrun): add anyrun flake

--- flake.nix
+++ flake.nix
@@ -38,6 +38,11 @@
       url = "github:nix-community/flake-firefox-nightly";
       inputs.nixpkgs.follows = "nixpkgs";
     };
+    # A wayland native krunner-like runner, made with customizability in mind.
+    anyrun = {
+      url = "github:Kirottu/anyrun";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
   };
 
   outputs = inputs @ {
@@ -61,10 +66,12 @@
       extra-substituters = [
         "https://nix-community.cachix.org"
         "https://hyprland.cachix.org"
+        "https://anyrun.cachix.org"
       ];
       extra-trusted-public-keys = [
         "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs"
         "hyprland.cachix.org-1:a7pgxzMz7+chwVL3/pzj6jIBMioiJM7ypFP8PwtkuGc="
+        "anyrun.cachix.org-1:pqBobmOjI7nKlsUMV25u9QHa9btJK65/C8vnO3p346s="
       ];
     };
 
