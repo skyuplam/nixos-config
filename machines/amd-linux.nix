ce6240f76465161c9696b54e8b655d48fd4b2da7
Terrence Lam <terrence@tlam.dev>
2024-01-28 13:30:14 +0100

    feat(nixos): setup fonts

--- machines/amd-linux.nix
+++ machines/amd-linux.nix
@@ -29,5 +29,43 @@
     };
   };
 
+  fonts = {
+    # use fonts specified by user rather than default ones
+    enableDefaultPackages = false;
+    fontDir.enable = true;
+
+    packages = with pkgs; [
+      # icon fonts
+      material-design-icons
+      font-awesome
+
+      noto-fonts-emoji
+      source-sans
+      source-serif
+      source-han-sans
+      source-han-serif
+
+      # nerdfonts
+      # https://github.com/NixOS/nixpkgs/blob/nixos-23.11/pkgs/data/fonts/nerdfonts/shas.nix
+      (nerdfonts.override {
+        fonts = [
+          # symbols icon only
+          "NerdFontsSymbolsOnly"
+          "JetBrainsMono"
+        ];
+      })
+      dejavu_fonts
+    ];
+    # user defined fonts
+    # the reason there's Noto Color Emoji everywhere is to override DejaVu's
+    # B&W emojis that would sometimes show instead of some Color emojis
+    fontconfig.defaultFonts = {
+      serif = ["Source Han Serif TC" "Noto Color Emoji"];
+      sansSerif = ["Source Han Sans TC" "Noto Color Emoji"];
+      monospace = ["JetBrainsMono Nerd Font" "Noto Color Emoji"];
+      emoji = ["Noto Color Emoji"];
+    };
+  };
+
   networking.hostName = "tlamws";
 }
