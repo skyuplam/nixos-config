becd4c1510b72ce99e88a259d5418f5e4abf67c8
Terrence Lam <terrence@tlam.dev>
2024-02-09 21:06:00 +0100

    feat(nvim): add icons for Codeium & Copilot

--- users/terrencelam/config/nvim/lua/tl/completion.lua
+++ users/terrencelam/config/nvim/lua/tl/completion.lua
@@ -12,7 +12,7 @@ if has_lspkind then
     mode = 'symbol', -- show only symbol annotations
     maxwidth = 50, -- prevent the popup from showing more than provided characters (e.g 50 will not show more than 50 characters)
     ellipsis_char = '...',
-    symbol_map = { Codeium = '' },
+    symbol_map = { Codeium = '󰘦 ', Copilot = '' },
   })
 end
 
