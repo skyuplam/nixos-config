e5e3ba2cac402f2c31e0c069ec290d929437fbd1
Terrence Lam <terrence@tlam.dev>
2024-05-03 09:54:31 +0200

    chore(nvim): adjust treesitter-context config

--- users/terrencelam/config/nvim/lua/tl/treesitter.lua
+++ users/terrencelam/config/nvim/lua/tl/treesitter.lua
@@ -129,7 +129,7 @@ end
 local function setup_context()
   ts_context.setup({
     enable = true, -- Enable this plugin (Can be enabled/disabled later via commands)
-    max_lines = 0, -- How many lines the window should span. Values <= 0 mean no limit.
+    max_lines = 5, -- How many lines the window should span. Values <= 0 mean no limit.
     patterns = { -- Match patterns for TS nodes. These get wrapped to match at word boundaries.
       -- For all filetypes
       -- Note that setting an entry here replaces all other patterns for this entry.
@@ -151,6 +151,15 @@ local function setup_context()
       --       'impl_item',
       --   },
     },
+    min_window_height = 0, -- Minimum editor window height to enable context. Values <= 0 mean no limit.
+    line_numbers = true,
+    multiline_threshold = 20, -- Maximum number of lines to show for a single context
+    trim_scope = 'outer', -- Which context lines to discard if `max_lines` is exceeded. Choices: 'inner', 'outer'
+    mode = 'cursor', -- Line used to calculate context. Choices: 'cursor', 'topline'
+    -- Separator between context and content. Should be a single character string, like '-'.
+    -- When separator is set, the context will only show up when there are at least 2 lines above cursorline.
+    separator = nil,
+    on_attach = nil, -- (fun(buf: integer): boolean) return false to disable attaching
     exact_patterns = {
       -- Example for a specific filetype with Lua patterns
       -- Treat patterns.rust as a Lua pattern (i.e "^impl_item$" will
