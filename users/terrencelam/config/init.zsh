87152438df7a0402cd4005875593877361190aa5
Terrence Lam <terrence@tlam.dev>
2024-06-15 09:01:12 +0100

    feat(zsh): local bin

--- users/terrencelam/config/init.zsh
+++ users/terrencelam/config/init.zsh
@@ -2,6 +2,7 @@
 # Add Completion sources
 # ------------------
 [ -d $HOME/.zfunc ] && fpath=($HOME/.zfunc $fpath)
+[ -d $HOME/.local/bin ] && fpath=($HOME/.local/bin $fpath)
 [ -d $HOME/dev/nix-zsh-completions ] && fpath=($HOME/dev/nix-zsh-completions $fpath)
 [ -d /etc/profiles/per-user/terrencelam/share/zsh/site-functions ] && fpath=(/etc/profiles/per-user/terrencelam/share/zsh/site-functions $fpath)
 [ -d /usr/local/share/zsh/site-functions ] && fpath=(/usr/local/share/zsh/site-functions $fpath)
