b124d7f980d34d0e21ea65837b41f90509f12451
Terrence Lam <terrence@tlam.dev>
2025-01-02 23:56:50 +0100

    feat(nvim): harper_ls linters settings

--- users/terrencelam/programs/dev/doc.nix
+++ users/terrencelam/programs/dev/doc.nix
@@ -19,6 +19,29 @@
 
           harper_ls = {
             enable = true;
+
+            settings = {
+              "harper-ls" = {
+                linters = {
+                  spell_check = true;
+                  spelled_numbers = false;
+                  an_a = true;
+                  sentence_capitalization = true;
+                  unclosed_quotes = true;
+                  wrong_quotes = false;
+                  long_sentences = true;
+                  repeated_words = true;
+                  spaces = true;
+                  matcher = true;
+                  correct_number_suffix = true;
+                  number_suffix_capitalization = true;
+                  multiple_sequential_pronouns = true;
+                  linking_verbs = false;
+                  avoid_curses = true;
+                  terminating_conjunctions = true;
+                };
+              };
+            };
           };
         };
 
