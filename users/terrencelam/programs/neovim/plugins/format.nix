47a532f1190eb8663bff99c922e08c16ef9ab84b
Terrence Lam <terrence@tlam.dev>
2025-01-29 09:59:15 +0100

    fix(nvim): disable codespell auto fix

--- users/terrencelam/programs/neovim/plugins/format.nix
+++ users/terrencelam/programs/neovim/plugins/format.nix
@@ -14,7 +14,7 @@
             cpp = ["clang-format"];
             rust = ["rustfmt"];
             # -- Use the "*" filetype to run formatters on all filetypes.
-            "*" = ["codespell"];
+            # "*" = ["codespell"];
             # -- Use the "_" filetype to run formatters on filetypes that don't
             # -- have other formatters configured.
             "_" = ["trim_whitespace"];
