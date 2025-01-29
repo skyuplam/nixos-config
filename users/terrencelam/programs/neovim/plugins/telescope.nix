d385502bc32ebb3a6950c1b6ab34e87cc6af8d9a
Terrence Lam <terrence@tlam.dev>
2025-01-29 09:58:38 +0100

    feat(nvim): grep word

--- users/terrencelam/programs/neovim/plugins/telescope.nix
+++ users/terrencelam/programs/neovim/plugins/telescope.nix
@@ -28,10 +28,35 @@
             fuzzy = true;
             override_generic_sorter = true; # override the generic sorter
             override_file_sorter = true; # override the file sorter
-            case_mode = "smart_case"; # or "ignore_case" or "respect_case"
+            case_moe = "smart_case"; # or 'ignore_case' or 'respect_case'
           };
         };
-        live-grep-args.enable = true;
+        live-grep-args = {
+          enable = true;
+          settings = {
+            auto_quoting = true;
+            mappings = {
+              i = {
+                "<C-k>" = {
+                  __raw = ''
+                    require("telescope-live-grep-args.actions").quote_prompt()
+                  '';
+                };
+                "<C-i>" = {
+                  __raw = ''
+                    require("telescope-live-grep-args.actions").quote_prompt({ postfix = " --iglob " })
+                  '';
+                };
+                # freeze the current list a start a fuzzy search in the frozen list
+                "<C-space>" = {
+                  __raw = ''
+                    require("telescope.actions").to_fuzzy_refine
+                  '';
+                };
+              };
+            };
+          };
+        };
         manix.enable = true;
         media-files.enable = true;
         ui-select.enable = true;
@@ -110,6 +135,14 @@
           action = "<cmd>lua require('telescope').extensions.live_grep_args.live_grep_args({ search_dirs = { '%:p:h' } })<CR>";
           desc = "Grep files from current directory";
         };
+        "<leader>fw" = {
+          action = "<cmd>lua require('telescope-live-grep-args.shortcuts').grep_word_under_cursor()<CR>";
+          desc = "Grep with word under cursor";
+        };
+        "<leader>fW" = {
+          action = "<cmd>lua require('telescope-live-grep-args.shortcuts').grep_word_under_cursor_current_buffer()<CR>";
+          desc = "Grep with word under cursor for the current buffer";
+        };
         "<leader>fr" = {
           action = "<cmd>Telescope registers<CR>";
           desc = "Registers";
@@ -161,6 +194,22 @@
         "<leader>go" = {
           action = "<cmd>Telescope aerial<CR>";
           desc = "Outline Symbols";
+        };
+      }
+      ++ lib.mapAttrsToList (key: mapping: {
+        mode = "v";
+        inherit key;
+        action =
+          mapping.action;
+        options.desc = mapping.desc;
+      }) {
+        "<leader>fw" = {
+          action = "<cmd>lua require('telescope-live-grep-args.shortcuts').grep_visual_selection()<CR>";
+          desc = "Grep with selection";
+        };
+        "<leader>fW" = {
+          action = "<cmd>lua require('telescope-live-grep-args.shortcuts').grep_word_visual_selection_current_buffer()<CR>";
+          desc = "Grep with selection for the current buffer";
         };
       };
   };
