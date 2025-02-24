69a72bf9e34c67068c2c96ca8463cafa08a144fe
Terrence Lam <terrence@tlam.dev>
2025-02-24 11:02:51 +0100

    chore: disable nix.enable
    Move to Determinate Systems
    
    As Determinate manages the Nix installation itself, you will need to set
    `nix.enable = false;` in your configuration to disable nix-darwin’s own
    Nix management.

--- machines/macbook-pro-m2.nix
+++ machines/macbook-pro-m2.nix
@@ -1,7 +1,12 @@
-{ config, pkgs, ... }: {
+{
+  config,
+  pkgs,
+  ...
+}: {
   # We install Nix using a separate installer so we don't want nix-darwin
   # to manage it for us. This tells nix-darwin to just use whatever is running.
-  nix.useDaemon = true;
+  # nix.useDaemon = true;
+  nix.enable = false;
 
   # Keep in async with vm-shared.nix. (todo: pull this out into a file)
   nix = {
@@ -22,9 +27,9 @@
       . '/nix/var/nix/profiles/default/etc/profile.d/nix-daemon.sh'
     fi
     # End Nix
-    '';
+  '';
 
-  environment.shells = with pkgs; [ bashInteractive zsh ];
+  environment.shells = with pkgs; [bashInteractive zsh];
   environment.systemPackages = with pkgs; [
   ];
 }
