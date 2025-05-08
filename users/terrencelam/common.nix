6def408282c39e03aead196ed9f0c1adbc56a04d
Terrence Lam <terrence@tlam.dev>
2025-05-08 09:14:29 +0200

    chore: add common.nix for common stuff

---
+++ users/terrencelam/common.nix
@@ -0,0 +1,18 @@
+{inputs, ...}: let
+  secretspath = builtins.toString inputs.nix-secrets;
+in {
+  sops = {
+    defaultSopsFile = "${secretspath}/secrets.yaml";
+    age = {
+      sshKeyPaths = ["/etc/ssh/ssh_host_ed25519_key"];
+      keyFile = "/var/lib/sops-nix/key.txt";
+      generateKey = true;
+    };
+    secrets = {
+      wg0 = {
+        preshared_key = {};
+        private_key = {};
+      };
+    };
+  };
+}
