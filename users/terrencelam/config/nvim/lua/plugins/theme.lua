0d0e803c0950e683b2dc66eb7bad9c1b9557106a
Terrence Lam <terrence@tlam.dev>
2025-08-05 15:11:15 +0200

    fix(nvim): catppuccin float transparent

--- users/terrencelam/config/nvim/lua/plugins/theme.lua
+++ users/terrencelam/config/nvim/lua/plugins/theme.lua
@@ -3,6 +3,10 @@ return {
   name = 'catppuccin',
   opts = {
     transparent_background = true,
+    float = {
+      transparent = true, -- enable transparent floating windows
+      solid = true, -- use solid styling for floating windows, see |winborder|
+    },
     default_integrations = false,
     integrations = {
       telescope = {
