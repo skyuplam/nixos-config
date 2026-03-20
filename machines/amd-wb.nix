7f986cdffbfbca212f9e69a607677f354e56e946
Terrence Lam <terrence@tlam.dev>
2026-03-20 22:36:27 +0100

    fix: add stateVersion

--- machines/amd-wb.nix
+++ machines/amd-wb.nix
@@ -19,6 +19,8 @@
     fileSystems = ["/"];
   };
 
+  services.libinput.enable = true;
+
   networking = {
     hostName = "tlamwb";
     networkmanager.enable = true;
@@ -34,4 +36,12 @@
     priority = 5;
     memoryPercent = 50;
   };
+
+  # This value determines the NixOS release from which the default
+  # settings for stateful data, like file locations and database versions
+  # on your system were taken. It‘s perfectly fine and recommended to leave
+  # this value at the release version of the first install of this system.
+  # Before changing this value read the documentation for this option
+  # (e.g. man configuration.nix or on https://nixos.org/nixos/options.html).
+  system.stateVersion = "25.11"; # Did you read the comment?
 }
