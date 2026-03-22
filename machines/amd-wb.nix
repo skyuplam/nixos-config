229ff6e17e757db07d480293cd2eb5d6e5432335
Terrence Lam <terrence@tlam.dev>
2026-03-22 22:40:20 +0100

    feat(amd-wb): AMD hardware and Motorcomm YT6801
    Refs https://github.com/NixOS/nixos-hardware/pull/1676

--- machines/amd-wb.nix
+++ machines/amd-wb.nix
@@ -1,4 +1,8 @@
-{...}: {
+{
+  config,
+  lib,
+  ...
+}: {
   imports = [
     ./hardware/amd-wb.nix
     ./disko-config-wb.nix
@@ -37,11 +41,31 @@
     memoryPercent = 50;
   };
 
+  # From https://github.com/NixOS/nixos-hardware/pull/1676
+  # Add Tuxedo InfinityBook Pro 14 Gen 10 hardware specifics
+  # Add Motorcomm YT6801 Driver if available
+  boot.extraModulePackages = with config.boot; [
+    (
+      if (kernelPackages ? yt6801)
+      then kernelPackages.yt6801
+      else null
+    )
+    # Enables the zenpower sensor in lieu of the k10temp sensor on Zen CPUs https://git.exozy.me/a/zenpower3
+    # On Zen CPUs zenpower produces much more data entries
+    # Refs. https://github.com/cguentherTUChemnitz/nixos-hardware/blob/6cdf1d899c48f7a9f16bf422b93a56271f473ea4/common/cpu/amd/zenpower.nix
+    kernelPackages.zenpower
+  ];
+
+  boot.blacklistedKernelModules = ["k10temp"];
+  # Enables the amd cpu scaling https://www.kernel.org/doc/html/latest/admin-guide/pm/amd-pstate.html
+  # On recent AMD CPUs this can be more energy efficient.
+  boot.kernelModules = ["zenpower" "amd_pstate=active"];
+
   # This value determines the NixOS release from which the default
   # settings for stateful data, like file locations and database versions
   # on your system were taken. It‘s perfectly fine and recommended to leave
   # this value at the release version of the first install of this system.
   # Before changing this value read the documentation for this option
   # (e.g. man configuration.nix or on https://nixos.org/nixos/options.html).
-  system.stateVersion = "25.11"; # Did you read the comment?
+  system.stateVersion = "26.05"; # Did you read the comment?
 }
