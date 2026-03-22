25875587b15232c80ddca66b66b78a8462487a30
Terrence Lam <terrence@tlam.dev>
2026-03-22 21:45:29 +0100

    fix: home-manager stateVersion

--- users/terrencelam/home-manager/amd-wb.nix
+++ users/terrencelam/home-manager/amd-wb.nix
@@ -15,6 +15,15 @@
     })
   ];
 
+  home = {
+    # This value determines the Home Manager release that your configuration is compatible with. This
+    # helps avoid breakage when a new Home Manager release introduces backwards incompatible changes.
+    #
+    # You can update Home Manager without changing this value. See the Home Manager release notes for
+    # a list of state version changes in each release.
+    stateVersion = "26.05";
+  };
+
   services = {
     swayidle = let
       lock = "${pkgs.dms-shell}/bin/dms ipc call lock lock";
