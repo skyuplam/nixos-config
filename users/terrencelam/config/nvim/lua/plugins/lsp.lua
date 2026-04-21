988a27cae4d2866215e75719592a04584e9d75e0
Terrence Lam <terrence@tlam.dev>
2026-04-21 22:33:54 +0200

    fix(nvim): codelens.refresh

--- users/terrencelam/config/nvim/lua/plugins/lsp.lua
+++ users/terrencelam/config/nvim/lua/plugins/lsp.lua
@@ -222,10 +222,12 @@ return {
     -- Code lens
     if opts.codelens.enabled and vim.lsp.codelens then
       lsp.on_supports_method('textDocument/codeLens', function(_, buffer)
-        vim.lsp.codelens.refresh()
+        vim.lsp.codelens.enable(true, { bufnr = buffer })
         vim.api.nvim_create_autocmd({ 'BufEnter', 'CursorHold', 'InsertLeave' }, {
           buffer = buffer,
-          callback = vim.lsp.codelens.refresh,
+          callback = function()
+            vim.lsp.codelens.enable(true, { bufnr = buffer })
+          end,
         })
       end)
     end
