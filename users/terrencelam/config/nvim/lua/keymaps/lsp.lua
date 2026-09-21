4cd2f3ba3191c238efe0c903f8acd86ab7677e9e
Terrence Lam <terrence@tlam.dev>
2026-09-21 11:44:32 +0200

    fix: deprecated client.supports_method

--- users/terrencelam/config/nvim/lua/keymaps/lsp.lua
+++ users/terrencelam/config/nvim/lua/keymaps/lsp.lua
@@ -41,7 +41,7 @@ function M.has(buffer, method)
   method = method:find('/') and method or 'textDocument/' .. method
   local clients = vim.lsp.get_clients({ bufnr = buffer })
   for _, client in ipairs(clients) do
-    if client.supports_method(method, buffer) then
+    if client:supports_method(method, buffer) then
       return true
     end
   end
