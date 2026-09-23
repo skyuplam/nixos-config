a5aec8c6f6cbdbd5667da6189e796d844c70b6af
Terrence Lam <terrence@tlam.dev>
2026-09-23 12:17:55 +0200

    build: rustaceanvim v9

--- users/terrencelam/config/nvim/lua/plugins/rust.lua
+++ users/terrencelam/config/nvim/lua/plugins/rust.lua
@@ -1,7 +1,26 @@
 return {
   {
     'mrcjkb/rustaceanvim',
-    version = '^6', -- Recommended
+    version = '^9', -- Recommended
     lazy = false, -- This plugin is already lazy
+    config = function(lazy, opts)
+      vim.g.rustaceanvim = {
+        ---@type rustaceanvim.tools.Opts
+        tools = {},
+        ---@type rustaceanvim.lsp.ClientOpts
+        server = {
+          default_settings = {
+            -- rust-analyzer language server configuration
+            ['rust-analyzer'] = {
+              -- diagnostics = {
+              --   disabled = { 'unlinked-file' },
+              -- },
+            },
+          },
+        },
+        ---@type rustaceanvim.dap.Opts
+        dap = {},
+      }
+    end,
   },
 }
