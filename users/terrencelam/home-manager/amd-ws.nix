95398a5bd744d2e2bb0b79c7c5dd0dfe9688a748
Terrence Lam <terrence@tlam.dev>
2026-09-23 10:29:08 +0200

    build: add remmina for RDP

--- users/terrencelam/home-manager/amd-ws.nix
+++ users/terrencelam/home-manager/amd-ws.nix
@@ -18,6 +18,7 @@
   home = {
     packages = with pkgs; [
       samba
+      remmina
     ];
     # This value determines the Home Manager release that your configuration is compatible with. This
     # helps avoid breakage when a new Home Manager release introduces backwards incompatible changes.
